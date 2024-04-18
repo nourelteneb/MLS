ba6337765bd4edf7159aeff81b71fcdb4105d8af
nourelteneb <167203920+nourelteneb@users.noreply.github.com>
2024-04-18 03:20:32 +0200

    StartScene

---
+++ Assets/Scenes/startscene.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startscene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
