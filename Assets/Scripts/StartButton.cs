452f3e5a38197236162be89f9865d2b3107b182d
nourelteneb <167203920+nourelteneb@users.noreply.github.com>
2024-04-22 02:29:03 +0200

    Voiceover

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class StartButton : MonoBehaviour
 {
     public Animator animator;
+    public AudioSource aud;
     // Start is called before the first frame update
     void Start()
     {
-        
-}
+        aud = GetComponent<AudioSource>();
+    }
 
     // Update is called once per frame
     public void PlayButtonAnimation()
     {
         animator.Play("StartButton");
-}
+        aud.Play();
+    }
 }
